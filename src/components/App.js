0d88e7bb35cde2b5b07b48b7fad6a8527ce6eea7
Laxmikant Shinde <laxmikant251102@gmail.com>
2025-11-30 12:30:48 +0530

    task done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 
 import React from "react";
+import { connect } from 'react-redux';
+import { increment, decrement } from '../redux/actions';
 import './../styles/App.css';
 
-const App = () => {
+const App = ({ count, increment, decrement }) => {
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>{count}</h1>
+        <button onClick={increment}>increment</button>
+        <button onClick={decrement}>decrement</button>
     </div>
   )
 }
 
-export default App
+// Map Redux state to component props
+const mapStateToProps = (state) => ({
+  count: state.count
+});
+
+// Map Redux actions to component props
+const mapDispatchToProps = {
+  increment,
+  decrement
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
---
+++ src/redux/actionTypes.js
@@ -0,0 +1,3 @@
+// Action types for counter
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
